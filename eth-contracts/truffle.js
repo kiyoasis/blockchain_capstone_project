bc4c41546584a3e6f6e218a71db8497eb07c5259
Kiyoshi Nakayama <knakayama@em19-nb1.nec-labs.com>
2019-04-26 18:11:32 -0700

    miscellaneous

---
+++ eth-contracts/truffle.js
@@ -0,0 +1,24 @@
+// var HDWalletProvider = require("truffle-hdwallet-provider");
+// var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
+module.exports = {
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: "*"
+    }
+    // development: {
+    //   provider: function() {
+    //     return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+    //   },
+    //   network_id: '*',
+    //   //gas: 5999999
+    // }
+  },
+  compilers: {
+    solc: {
+      version: "^0.5.0"
+    }
+  }
+};
