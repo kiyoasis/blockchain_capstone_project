57be90993db935892fd4b976c33b467441baa99f
Kiyoshi Nakayama <knakayama@em18-nb1.local>
2019-04-29 00:17:04 -0700

    dapp client server codes implemented

---
+++ src/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Hapi = require('hapi');
+const PORT = 8080; //8545;
+
+// Create a server with a host and port
+const server = Hapi.server({
+    host: 'localhost',
+    port: PORT
+});
+
+// Add the route
+const start = async () => {
+
+    await server.register(require('inert'));
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, h) {
+
+            return h.file('src/index.html');
+        }
+    });
+
+    await server.start();
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
