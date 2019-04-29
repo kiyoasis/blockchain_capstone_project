945d96eea0a58f262a5ee9a7503704f018253008
Kiyoshi Nakayama <knakayama@em18-nb1.local>
2019-04-29 00:15:17 -0700

    build changed

--- eth-contracts/truffle.js
+++ eth-contracts/truffle.js
@@ -1,5 +1,5 @@
-// var HDWalletProvider = require("truffle-hdwallet-provider");
-// var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
 
 module.exports = {
   networks: {
@@ -7,14 +7,13 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: "*"
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/a2143633373a45b1b2cee31f31b1d8e3'),
+      network_id: 4,
+      gas: 6700000,
+      gasPrice: 10000000000,
     }
-    // development: {
-    //   provider: function() {
-    //     return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-    //   },
-    //   network_id: '*',
-    //   //gas: 5999999
-    // }
   },
   compilers: {
     solc: {
